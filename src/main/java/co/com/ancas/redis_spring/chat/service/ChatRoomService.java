c6d872910bbbbdf9eb4e404d8ef7afaacab23c8d
afcastro <afcastro@cotrafa.com.co>
2025-05-27 10:07:02 -0500

    Feat: finish chat application

--- src/main/java/co/com/ancas/redis_spring/chat/service/ChatRoomService.java
+++ src/main/java/co/com/ancas/redis_spring/chat/service/ChatRoomService.java
@@ -1,5 +1,6 @@
 package co.com.ancas.redis_spring.chat.service;
 
+import org.redisson.api.RListReactive;
 import org.redisson.api.RTopicReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.client.codec.StringCodec;
@@ -24,14 +25,17 @@ public class ChatRoomService implements WebSocketHandler {
     public Mono<Void> handle(WebSocketSession session) {
         String room = getChatRoom(session);
         RTopicReactive topic = this.client.getTopic(room, StringCodec.INSTANCE);
+        RListReactive<String> history=this.client.getList("history:" + room, StringCodec.INSTANCE);
 
         session.receive()
                 .map(WebSocketMessage::getPayloadAsText)
-                .flatMap(msg -> topic.publish(msg))
+                .flatMap(msg -> history.add(msg).then(topic.publish(msg)))
                 .doOnError(System.out::println)
                 .doFinally(s -> System.out.println("Subscriber finally called: " + s))
                 .subscribe();
+
         Flux<WebSocketMessage> flux = topic.getMessages(String.class)
+                .startWith(history.iterator())
                 .map(session::textMessage)
                 .doOnError(System.out::println)
                 .doFinally(s -> System.out.println("Subscriber finally called: " + s));
